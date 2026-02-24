4e2dc466cbea95b267d4f351261b3e0c0450a2d0
Mastertechsolutionscenter <harmonyamanifoundation@gmail.com>
2026-02-24 09:12:56 +0300

    Update page.tsx

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -267,7 +267,8 @@ export default function AboutUsPage() {
               href="tel:+254727717019"
               className="bg-transparent border border-white text-white font-bold py-3 px-6 rounded-full hover:bg-white/10 transition duration-300 inline-flex items-center"
             >
-              <Phone className='w-5 h-5 mr-2' />
+              <Phone className='w-5 h-5 mr-2' a
+              href="tel:+254727717019" />
               Call/WhatsApp Us
             </a>
           </div>
